8d43a9be7d7c4a3615e9ab27c1c3a3a175c683ed
jaredfreedman <freedmaj@oregonstate.edu>
2021-10-12 10:38:31 -0700

    Update coding_demo.R

--- Code/coding_demo.R
+++ Code/coding_demo.R
@@ -1,2 +1,26 @@
 # Analytical workflows : coding demo
 
+#Additional info about code useful for 
+#collaborators or future self
+
+
+
+# Clear workspace and close all graphics ----------------------------------
+# Start script from a clean workspace to prevent errors
+
+rm(list = ls())
+graphics.off()
+
+
+
+# Generate some data ------------------------------------------------------
+
+n <- 100 #numbers
+m <- 2.2 #slope
+b <- 1.1 #intercept
+
+x <- rnorm(n = n, mean = 0, sd = 1) #indedpendent variable
+noise <- rnorm(n = n, mean = 0, sd = 1)
+y <- m * x + b + noise
+
+plot(x,y)
